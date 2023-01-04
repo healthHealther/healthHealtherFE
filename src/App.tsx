ead873fee773fdb740c1fb0019724f6973158831
kykson2 <kykson2@naver.com>
2023-01-04 13:22:16 +0900

    eslint 수정

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">HI Dev </h1>
+      <h1 className="text-3xl font-bold underline text-red-200">HI Dev </h1>
     </div>
   );
 }
