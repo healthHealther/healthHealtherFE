4da41d43deb475677f1864a2870abbf7e4d52a0b
김관우 <ganwoo98@gmail.com>
2023-01-09 13:08:48 +0900

    Add: 메인페이지의 SpaceContentPreview  및 SpaceContentsList 컨포넌트 추가

--- src/page/home/HomePage.tsx
+++ src/page/home/HomePage.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import SpaceContentPreview from "../../components/spaceRent/SpaceContentPreview";
 import NavBar from "../../components/NavBar";
 import TopBar from "../../components/TopBar";
 
@@ -7,8 +8,8 @@ export default function HomePage() {
   return (
     <div>
       <TopBar pageTitle="home" />
-      <div>
-        HomePage
+      <div className="max-w-[475px] min-w-[390px] sm:mx-auto mt-[48px]">
+        {/* HomePage
         <div className="mt-4">
           <Link className="mx-4" to="spaceRent">
             운동공간대여
@@ -22,7 +23,8 @@ export default function HomePage() {
           <Link className="mx-4" to="login">
             로그인
           </Link>
-        </div>
+        </div> */}
+        <SpaceContentPreview />
         <NavBar pageTitle={"home"} />
       </div>
     </div>
