334111a5aa306be36a0d961b34efab77252e9277
chy2love <chlgkduddlek@gmail.com>
2023-01-09 17:51:48 +0900

    Mod: 홈짐 미리보기 더보기 아이콘 업데이트

--- src/components/spaceRent/SpaceContentPreview.tsx
+++ src/components/spaceRent/SpaceContentPreview.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import SpaceContents from "./SpaceContentsList";
-
+import chevronIcon from "../../assets/chevron.svg";
 export default function SpaceContentPreview() {
   return (
-    <article className="mx-[20px]">
+    <article className="px-[20px]">
       <div className="flex h-[24px] justify-between ">
         <div className="text-lg font-bold">오늘의 홈짐 추천</div>
         <Link to={"/spaceRent"} className="flex items-center">
           <span className="text-sm">더보기</span>
-          <p className="w-[6px] h-[6px] border-t-2 border-r-2 border-solid border-black origin-center rotate-45"></p>
+          <div>
+            <img src={chevronIcon} alt="" />
+          </div>
         </Link>
       </div>
       <SpaceContents />
