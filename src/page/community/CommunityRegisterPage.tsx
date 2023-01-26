6218c31ab53c31a919e86bd00825bc9d05d40987
chy2love <chlgkduddlek@gmail.com>
2023-01-26 15:11:56 +0900

    Add: 커뮤니티 등록 페이지

--- src/page/community/CommunityRegisterPage.tsx
+++ src/page/community/CommunityRegisterPage.tsx
@@ -1,11 +1,13 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import TopBar from "../../components/TopBar";
+import ConfirmPopUp from "../../components/ConfirmPopUp";
 
 export default function CommunityRegisterPage() {
   const navigate = useNavigate();
   const [active, setActive] = useState(false);
+  const [filled, setFilled] = useState(false);
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const spaceReg = / /g;
@@ -18,10 +20,16 @@ export default function CommunityRegisterPage() {
     } else {
       setActive(false);
     }
+    if (
+      title?.replaceAll(spaceReg, "").length !== 0 ||
+      description?.replaceAll(spaceReg, "").length !== 0
+    ) {
+      setFilled(true);
+    } else {
+      setFilled(false);
+    }
   }, [title, description]);
-  useEffect(() => {
-    console.log(active);
-  }, [active]);
+
   const handlePostBtn = async () => {
     if (
       title?.replaceAll(spaceReg, "").length !== 0 &&
@@ -46,61 +54,64 @@ export default function CommunityRegisterPage() {
     }
   };
   return (
-    <div className="relative max-w-[475px] min-w-[390px] sm:mx-auto pt-[48px] bg-white min-h-[100vh] px-[20px]">
+    <div className="relative max-w-[475px] min-w-[390px] sm:mx-auto  bg-white min-h-[100vh]">
+      <ConfirmPopUp active={filled} />
       <TopBar />
-      <p className="pt-[32px] font-bold text-[24px] mb-6">커뮤니티 등록</p>
-      <form action="">
-        <div className="mb-[24px]">
-          <label
-            htmlFor="newContentTitle"
-            className="w-[30px] h-[22px] text-[#8b8b8b] font-bold text-[14px] relative flex mb-[8px]"
+      <div className="px-[20px] pt-[48px]">
+        <p className="pt-[32px] font-bold text-[24px] mb-6">커뮤니티 등록</p>
+        <form action="">
+          <div className="mb-[24px]">
+            <label
+              htmlFor="newContentTitle"
+              className="w-[30px] h-[22px] text-[#8b8b8b] font-bold text-[14px] relative flex mb-[8px]"
+            >
+              제목
+              <div className="absolute w-1 h-1 bg-[#08bd9d] rounded-full top-0 right-0"></div>
+            </label>
+            <textarea
+              className="resize-none border rounded-[8px] h-11 text-[16px] px-[12px] py-[10px] overflow-hidden w-full"
+              rows={1}
+              onChange={(e) => {
+                setTitle(e.target.value);
+              }}
+              placeholder="제목을 입력해주세요"
+              required
+              name="Title"
+              id="newContentTitle"
+            ></textarea>
+          </div>
+          <div>
+            <label
+              htmlFor="newContentDescription"
+              className="w-[30px] h-[22px] text-[#8b8b8b] font-bold text-[14px] relative flex mb-[8px]"
+            >
+              내용
+              <div className="absolute w-1 h-1 bg-[#08bd9d] rounded-full top-0 right-0"></div>
+            </label>
+            <textarea
+              className="resize-none border rounded-[8px] h-[260px] text-[16px] px-[12px] py-[10px] overflow-hidden w-full"
+              onChange={(e) => {
+                setDescription(e.target.value);
+              }}
+              required
+              placeholder="내용을 입력해주세요"
+              name="Description"
+              id="newContentDescription"
+            ></textarea>
+          </div>
+          <button
+            type="button"
+            onClick={handlePostBtn}
+            className={`fixed bottom-5 w-[435px] h-[48px] rounded-[8px] text-white ${
+              active
+                ? "bg-[#08bd9d] hover:bg-[#009d81]"
+                : "bg-[#c4c4c4] hover:bg-[#a5a5a5]"
+            }`}
           >
-            제목
-            <div className="absolute w-1 h-1 bg-[#08bd9d] rounded-full top-0 right-0"></div>
-          </label>
-          <textarea
-            className="resize-none border rounded-[8px] h-11 text-[16px] px-[12px] py-[10px] overflow-hidden w-full"
-            rows={1}
-            onChange={(e) => {
-              setTitle(e.target.value);
-            }}
-            placeholder="제목을 입력해주세요"
-            required
-            name="Title"
-            id="newContentTitle"
-          ></textarea>
-        </div>
-        <div>
-          <label
-            htmlFor="newContentDescription"
-            className="w-[30px] h-[22px] text-[#8b8b8b] font-bold text-[14px] relative flex mb-[8px]"
-          >
-            내용
-            <div className="absolute w-1 h-1 bg-[#08bd9d] rounded-full top-0 right-0"></div>
-          </label>
-          <textarea
-            className="resize-none border rounded-[8px] h-[260px] text-[16px] px-[12px] py-[10px] overflow-hidden w-full"
-            onChange={(e) => {
-              setDescription(e.target.value);
-            }}
-            required
-            placeholder="내용을 입력해주세요"
-            name="Description"
-            id="newContentDescription"
-          ></textarea>
-        </div>
-        <button
-          type="button"
-          onClick={handlePostBtn}
-          className={`fixed bottom-5 w-[435px] h-[48px] rounded-[8px] text-white ${
-            active
-              ? "bg-[#08bd9d] hover:bg-[#009d81]"
-              : "bg-[#c4c4c4] hover:bg-[#a5a5a5]"
-          }`}
-        >
-          등록
-        </button>
-      </form>
+            등록
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
