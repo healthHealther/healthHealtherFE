705d8529c8daa09bbbfb2bdbcd60a68789599599
chy2love <chlgkduddlek@gmail.com>
2023-01-31 19:39:22 +0900

    New: spinner 추가

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Spinner() {
+  return (
+    <div className="rounded-full border-2 border-[#e8e1e1] border-r-[#08bd9d] w-[150px] h-[150px] animate-spin mx-auto my-auto"></div>
+  );
+}
