5b7503cc60cfc368c66bc645f7259732fff70405
chy2love <chlgkduddlek@gmail.com>
2023-02-02 13:15:32 +0900

    Mod: 로그인 redirect uri 변경

--- src/components/OAuth.tsx
+++ src/components/OAuth.tsx
@@ -1,5 +1,5 @@
 export const KakaoAuth =
-  "https://kauth.kakao.com/oauth/authorize?client_id=6de4d2b244348ed33f66fc3d91d55e6d&redirect_uri=http://localhost:3000/login/callback/kakao&response_type=code&scope=profile_nickname";
+  "https://kauth.kakao.com/oauth/authorize?client_id=6de4d2b244348ed33f66fc3d91d55e6d&redirect_uri=health-healther-jrwm7anfv-healthhealther.vercel.app/login/callback/kakao&response_type=code&scope=profile_nickname";
 
 export const GoogleAuth =
-  "https://accounts.google.com/o/oauth2/v2/auth?scope=profile&response_type=code&client_id=899709469216-gr8606mpggciajq5qmaj3782uvs8fvmr.apps.googleusercontent.com&redirect_uri=http://localhost:3000/login/callback/google";
+  "https://accounts.google.com/o/oauth2/v2/auth?scope=profile&response_type=code&client_id=899709469216-gr8606mpggciajq5qmaj3782uvs8fvmr.apps.googleusercontent.com&redirect_uri=health-healther-jrwm7anfv-healthhealther.vercel.app/login/callback/google";
